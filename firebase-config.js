70dd04d7d9aae3ac7e19135a0a327caca8811157
binyamin543210-sys <binyamin543210@gmail.com>
2025-12-07 19:31:25 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+
+// Firebase config – תואם לדפדפן (compat)
+const firebaseConfig = {
+  apiKey: "AIzaSyCa808qwjJ8bayhjkTqZ8P9fRhfgi19xtY",
+  authDomain: "bnapp-ddcbf.firebaseapp.com",
+  databaseURL: "https://bnapp-ddcbf-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "bnapp-ddcbf",
+  storageBucket: "bnapp-ddcbf.appspot.com",
+  messagingSenderId: "523128255450",
+  appId: "1:523128255450:web:d29cdda3f21435f96686e3",
+  measurementId: "G-61DKZ1B5L2"
+};
+
+firebase.initializeApp(firebaseConfig);
---
+++ service-worker.js
@@ -0,0 +1,38 @@
+
+const CACHE_NAME = 'bnapp-calendar-lux-b2-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './firebase-config.js',
+  './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k))
+      )
+    )
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  const { request } = event;
+  if (request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(request).then((cached) => {
+      if (cached) return cached;
+      return fetch(request).catch(() => cached);
+    })
+  );
+});
